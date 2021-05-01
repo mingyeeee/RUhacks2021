1159d03a7d16c26e59da929e1e6e3cbc9f08d09d
rndmemex <rndmemex@protonmail.com>
2021-05-02 00:44:35 +0100

    vision.py new

--- computer-vision/vision.py
+++ computer-vision/vision.py
@@ -1,91 +1,75 @@
 import numpy as np
 import cv2 as cv
 
-font = cv.FONT_HERSHEY_SIMPLEX
-# points for 4 aruco points
-arucoPoints = np.empty([4, 4, 2], dtype=int)
-# points for homography
-rectanglepoints = np.empty([4, 2], dtype=int)
-cap = cv.VideoCapture(0)
+# initialize our cached reference points
+CACHED_REF_PTS = None
 
-#Load the dictionary that was used to generate the markers.
-dictionary = cv.aruco.Dictionary_get(cv.aruco.DICT_4X4_50)
+from imgsearch.aug_reality import find_and_warp
+from imutils.video import VideoStream
+from collections import deque
+import argparse
+import imutils
+import time
+import cv2
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", type=str, required=True, help="path to input video file for augmented reality")
+ap.add_argument("-c", "--cache", type=int, default=-1, help="whether or not to use reference points cache")
+args = vars(ap.parse_args())
+# load the ArUCo dictionary and grab the ArUCo parameters
+print("[INFO] initializing marker detector...")
+arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+arucoParams = cv2.aruco.DetectorParameters_create()
+# initialize the video file stream
+print("[INFO] accessing video stream...")
+vf = cv2.VideoCapture(args["input"])
+# initialize a queue to maintain the next frame from the video stream
+Q = deque(maxlen=128)
+# we need to have a frame in our queue to start our augmented reality
+# pipeline, so read the next frame from our video file source and add
+# it to our queue
+(grabbed, source) = vf.read()
+Q.appendleft(source)
+# initialize the video stream and allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
 
-# Initialize the detector parameters using default values
-parameters =  cv.aruco.DetectorParameters_create()
+# loop over the frames from the video stream
+while len(Q) > 0:
+	# grab the frame from our video stream and resize it
+	frame = vs.read()
+	frame = imutils.resize(frame, width=600)
+	# attempt to find the ArUCo markers in the frame, and provided
+	# they are found, take the current source image and warp it onto
+	# input frame using our augmented reality technique
+    # ----> simona
+	warped = find_and_warp(frame, source, cornerIDs=(1, 2, 3, 0), arucoDict=arucoDict, arucoParams=arucoParams, useCache=args["cache"] > 0)
 
-# x1,y1 are top left, x2,y2 are bottom right
-def get_grid_points(x1,y1,x2,y2):
-    xpoints = np.empty([6], dtype=int)
-    interval = int((x2-x1)/5)
-    xpoints[0] = x1
-    xpoints[1] = x1 + 1*interval
-    xpoints[2] = x1 + 2*interval
-    xpoints[3] = x1 + 3*interval
-    xpoints[4] = x1 + 4*interval
-    xpoints[5] = x2
-
-    ypoints = np.empty([5], dtype=int)
-    interval = int((y2-y1)/4)
-    ypoints[0] = x1
-    ypoints[1] = x1 + 1*interval
-    ypoints[2] = x1 + 2*interval
-    ypoints[3] = x1 + 3*interval
-    ypoints[4] = x2
-
-    coordinates = np.empty([5,4,2], dtype=int)
-    for i in range(4):
-        for j in range(5):
-            coordinates[j][i][0] = xpoints[j]
-            coordinates[j][i][1] = ypoints[i]
-
-    print(coordinates)
-    return coordinates
-
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    # Detect the markers in the image
-    markerCorners, markerIds, rejectedCandidates = cv.aruco.detectMarkers(frame, dictionary, parameters=parameters)
-
-    
-    if(len(markerCorners) > 0):
-        #print(markerCorners)
-        print(len(markerIds))
-        for i in range(len(markerIds)):
-            
-            for x in range(4):
-                arucoPoints[i][x][0] = int(markerCorners[i][0][x][0])
-                arucoPoints[i][x][1] = int(markerCorners[i][0][x][1])
-            p1 = (arucoPoints[i][0][0], arucoPoints[i][0][1])
-            p2 = (arucoPoints[i][1][0], arucoPoints[i][1][1])
-            p3 = (arucoPoints[i][2][0], arucoPoints[i][2][1])
-            p4 = (arucoPoints[i][3][0], arucoPoints[i][3][1])
-
-            rectanglepoints[i][0] = arucoPoints[i][0][0]
-            rectanglepoints[i][1] = arucoPoints[i][0][1]
-            
-            print(p1)
-            print(p2)
-            print(p3)
-            print(p4)
-            print('\n')
-            
-            frame = cv.line(frame,p1,p2,(255,0,0),5)
-            frame = cv.line(frame,p2,p3,(255,0,0),5)
-            frame = cv.line(frame,p3,p4,(255,0,0),5)
-            frame = cv.line(frame,p4,p1,(255,0,0),5)
-
-            id = str(int(markerIds[i]))
-            frame = cv.putText(frame,id,p1, font, 1,(0,255,0),2,cv.LINE_AA)
-    print(rectanglepoints)
-    
-    # Display the resulting frame
-    cv.imshow('frame',frame)
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
+	# if the warped frame is not None, then we know (1) we found the
+	# four ArUCo markers and (2) the perspective warp was successfully
+	# applied
+	if warped is not None:
+		# set the frame to the output augment reality frame and then
+		# grab the next video file frame from our queue
+		frame = warped
+		source = Q.popleft()
+	# for speed/efficiency, we can use a queue to keep the next video
+	# frame queue ready for us -- the trick is to ensure the queue is
+	# always (or nearly full)
+	if len(Q) != Q.maxlen:
+		# read the next frame from the video file stream
+		(grabbed, nextFrame) = vf.read()
+		# if the frame was read (meaning we are not at the end of the
+		# video file stream), add the frame to our queue
+		if grabbed:
+			Q.append(nextFrame)
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
