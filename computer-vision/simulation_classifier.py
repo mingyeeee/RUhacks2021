51f14e22e24a079e9e3746b700c600e2109d36d4
Mingye Chen <61096358+mingyeeee@users.noreply.github.com>
2021-05-02 01:43:18 -0400

    Add files via upload

---
+++ computer-vision/simulation_classifier.py
@@ -0,0 +1,68 @@
+import os
+import numpy as np
+
+import tensorflow as tf
+from tensorflow import keras
+assert tf.__version__.startswith('2')
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+
+# define a class that load and preprocess one image
+def load_process_image(file_path):
+
+  # Load image (in PIL image format by default)
+  image_original = load_img(file_path, target_size=(224, 224))
+  print("Image size after loading", image_original.size)
+
+  # Convert from numpy array
+  image_array = img_to_array(image_original)
+  print("Image size after converting to numpy array", image_array.shape)
+
+  # Expand dims to add batch size as 1
+  image_batch = np.expand_dims(image_array, axis=0)
+  print("Image size after expanding dimension", image_batch.shape)
+
+  # Preprocess image
+  image_preprocessed = tf.keras.applications.vgg16.preprocess_input(image_batch)
+
+  return image_original, image_preprocessed
+
+# 0 - Animal 1 - Nitrogen_Dioxide  2- Plant 3-circuit 4-earth
+def get_result(predictions):
+  return np.where(predictions == np.amax(predictions))
+
+# load model
+model = tf.keras.models.load_model('sim mode\saved_model')
+
+# Check its architecture
+model.summary()
+inputpath = 'input_images'
+start_inference = False
+stop = False
+
+predictions = np.empty([1,5], dtype=int)
+while(True):
+    # List all files in a directory using os.listdir
+    
+    for entry in os.listdir(inputpath):
+        if os.path.isfile(os.path.join(inputpath, entry)):
+            if(entry == "done.jpg"):
+                start_inference = True
+            if(entry == "stop.txt"):
+                stop = True
+
+    if(stop):
+        break
+    
+    if(start_inference):
+        for entry in os.listdir(inputpath):
+            if os.path.isfile(os.path.join(inputpath, entry)):
+                image_file_path = "input_images\\{entry}".format(entry=entry)
+
+                image_original, image_preprocessed = load_process_image(image_file_path)
+                os.remove(image_file_path)
+                predictions = model.predict(image_preprocessed)
+                # result = get_result(predictions[0])
+                print(predictions)
+        start_inference = False
+
--- computer-vision/vision.py
+++ computer-vision/vision.py
@@ -4,8 +4,21 @@ from imutils.video import VideoStream
 from collections import deque
 import argparse
 import imutils
-import time
+from time import time
 import cv2
+
+import os
+
+# List of simulation videos
+plant_vid = "./videos/plantcell.mov"
+animal_vid ="./videos/animalcell.mov"
+earth_vid = ""
+chemistry_vid = "./videos/Presentation1.mov"
+
+# List of splash screens
+plant_screen = "./splashscreens/plant.png"
+animal_screen = "./splashscreens/animal.png"
+chemistry_screen = "./splashscreens/N2O4.png"
 
 CACHED_REF_PTS = None
 #if statments while loop for each 
@@ -23,20 +36,73 @@ print("[INFO] initializing marker detector...")
 arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
 arucoParams = cv2.aruco.DetectorParameters_create()
 print("[INFO] accessing video stream...")
+
+# start web cam
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+#-------------------------------------------------------------------------------
+#             image scanning and sending to machine learning
+#-------------------------------------------------------------------------------
+while(True):
+    frame = vs.read()
+    cv2.putText(frame,"Please place your object in the box",(20,50), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,0,0),2,cv2.LINE_AA)
+    cv2.putText(frame,"Press q to start simulating",(60,80), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,0,0),2,cv2.LINE_AA)
+    
+    # for get a 224 by 224 pixel cropped image
+    height, width, channel = frame.shape
+    y = int(height/2) - 112
+    x = int(width/2) - 112
+    crop_img = frame[y:y+224, x:x+224]
+    #we can leave this out if we want to 
+    cv2.rectangle(frame, (x,y), (x+224, y+ 224),(255,0,0), 3)
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        # saving image for machine learning
+        cv2.imwrite("input_images\\done.jpg", crop_img)
+        break
+#-------------------------------------------------------------------------------
+# Check and wait for a response from the machine learning on what to simulate
+# -------------------------------------------------------------------------------
+response = False
+inputpath = 'input_images'
+
+while(not response):
+    frame = vs.read()
+    cv2.putText(frame,"Analyzing setup...",(200,200), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,0,0),2,cv2.LINE_AA)
+    cv2.imshow("Frame", frame)
+    for entry in os.listdir(inputpath):
+        if os.path.isfile(os.path.join(inputpath, entry)):
+            if(entry == 'response.json'):
+                os.remove("input_images\\response.json")
+                response = True
+    # Although theoretically unnecessary, the code breaks without :')
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+
+#-------------------------------------------------------------------------------
+#                           Start of the simulations
+# -------------------------------------------------------------------------------
+
 if args["type"] == 1: 
     #standard
-    video_path = "./videos/plantcell.mov"
+    # Change this
+    video_path = animal_vid
+    
     vf = cv2.VideoCapture(video_path)
     Q = deque(maxlen=128)
     (grabbed, source) = vf.read()
     Q.appendleft(source)
-    print("[INFO] starting video stream...")
-    vs = VideoStream(src=0).start()
-    time.sleep(2.0)
+    
     # loop over the frames from the video stream
     while len(Q) > 0:
         frame = vs.read()
-        frame = imutils.resize(frame, width=1000) #resize
+        # Change this
+        cv2.putText(frame,"Animal cell microscope",(130,50), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,0,0),2,cv2.LINE_AA)
+        cv2.putText(frame,"simulation",(220,80), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,0,0),2,cv2.LINE_AA)
+        #frame = imutils.resize(frame, width=1000) #resize
         warped = find_and_warp(frame, source, cornerIDs=(1, 2, 3, 0), arucoDict=arucoDict, arucoParams=arucoParams, useCache=args["cache"] > 0)
         if warped is not None:
             frame = warped
@@ -52,28 +118,42 @@ if args["type"] == 1:
     cv2.destroyAllWindows()
     vs.stop()
 #kinetic energy
-elif args["type"] == 2: 
-    video_path = "./videos/Presentation1.mov"
+elif args["type"] == 2:
+       
+    # loop over the frames from the video stream
+    # Change this
+    video_path = chemistry_vid
+
     vf = cv2.VideoCapture(video_path)
     Q = deque(maxlen=128)
     (grabbed, source) = vf.read()
     Q.appendleft(source)
-    print("[INFO] starting video stream...")
-    vs = VideoStream(src=0).start()
-    time.sleep(2.0)
-   # initialT = time()   
-    # loop over the frames from the video stream
+
+    initialT = time()
     while len(Q) > 0:
         frame = vs.read()
-        frame = imutils.resize(frame, width=1000) #resize
-        warped = find_and_warp(frame, source, cornerIDs=(1, 2, 3, 0), arucoDict=arucoDict, arucoParams=arucoParams, useCache=args["cache"] > 0)
-        if warped is not None:
-            frame = warped
-            source = Q.popleft()
-        if len(Q) != Q.maxlen:
-            (grabbed, nextFrame) = vf.read()
-            if grabbed:
-                Q.append(nextFrame)
+        # delay for shaking the paper. once 5 seconds has elapsed the animation will play
+        if(time() - initialT > 5):
+            #print("Enough energy has been added to the system!")
+            # Change this
+            cv2.putText(frame,"Enough energy has been ",(110,50), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,0,0),2,cv2.LINE_AA)
+            cv2.putText(frame,"added to the system!",(130,80), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,0,0),2,cv2.LINE_AA)
+            #frame = imutils.resize(frame, width=1000) #resize
+            warped = find_and_warp(frame, source, cornerIDs=(1, 2, 3, 0), arucoDict=arucoDict, arucoParams=arucoParams, useCache=args["cache"] > 0)
+            if warped is not None:
+                frame = warped
+                source = Q.popleft()
+            if len(Q) != Q.maxlen:
+                (grabbed, nextFrame) = vf.read()
+                if grabbed:
+                    Q.append(nextFrame)
+        # message when simulation hasn't started
+        else:
+            #print("Shake the paper to add energy to the system!") 
+            # Change this
+            cv2.putText(frame,"Shake the paper to add energy!",(70,80), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,0,0),2,cv2.LINE_AA)
+            cv2.putText(frame,"N2O4 and NO2 equilibrium simulation",(24,190), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,0,0),2,cv2.LINE_AA)
+            cv2.putText(frame,"Formation of N2O4 gas",(55,220), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,0,0),2,cv2.LINE_AA)
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
@@ -91,13 +171,12 @@ elif args["type"] == 3:
     Q = deque(maxlen=128)
     (grabbed, source) = vf.read()
     Q.appendleft(source)
-    print("[INFO] starting video stream...")
-    vs = VideoStream(src=0).start()
+
     time.sleep(2.0)
     # loop over the frames from the video stream
     while len(Q) > 0:
         frame = vs.read()
-        frame = imutils.resize(frame, width=1000) #resize
+        #frame = imutils.resize(frame, width=1000) #resize
         warped = find_and_warp(frame, source, cornerIDs=(1, 2, 3, 0), arucoDict=arucoDict, arucoParams=arucoParams, useCache=args["cache"] > 0)
         if warped is not None:
             frame = warped
